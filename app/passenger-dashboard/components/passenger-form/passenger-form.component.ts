3ff1260f06691bdb5fc2ff63e21908531461b176
Kaushik Chaubal <kaushikchaubal@gmail.com>
2018-07-09 16:59:34 +0100

    Binding forms to radio-buttons

--- app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
+++ app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
@@ -3,9 +3,9 @@ import { Component, Input } from '@angular/core';
 import { Passenger } from '../../models/passenger.interface';
 
 @Component({
-  selector: 'passenger-form',
-  styleUrls: ['passenger-form.component.scss'],
-  template: `
+    selector: 'passenger-form',
+    styleUrls: ['passenger-form.component.scss'],
+    template: `
     <form #form="ngForm" novalidate>
       {{ detail | json }}
 
@@ -25,11 +25,45 @@ import { Passenger } from '../../models/passenger.interface';
           [ngModel]="detail?.id">
       </div>
 
+      <div>
+        <label>
+          <input 
+            type="radio"
+            [value]="true"
+            name="checkedIn"
+            [ngModel]="detail?.checkedIn"
+            (ngModelChange)="toggleCheckIn($event)">
+          Yes
+        </label>
+        <label>
+          <input 
+            type="radio"
+            [value]="false"
+            name="checkedIn"
+            [ngModel]="detail?.checkedIn"
+            (ngModelChange)="toggleCheckIn($event)">
+          No
+        </label>
+      </div>
+
+      <div *ngIf="form.value.checkedIn">
+        Check in date:
+        <input 
+          type="number"
+          name="checkInDate"
+          [ngModel]="detail?.checkInDate">
+      </div>
+
       {{ form.value | json }}
     </form>
   `
 })
 export class PassengerFormComponent {
-  @Input()
-  detail: Passenger;
+    @Input()
+    detail: Passenger;
+    toggleCheckIn(checkedIn: boolean) {
+        if (checkedIn) {
+            this.detail.checkInDate = Date.now();
+        }
+    }
 }
