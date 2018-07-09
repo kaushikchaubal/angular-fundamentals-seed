e34914b482477c5a6415177eaafd1480836c1454
Kaushik Chaubal <kaushikchaubal@gmail.com>
2018-07-09 17:16:11 +0100

    Disabling the submit button

--- app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
+++ app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
@@ -4,9 +4,9 @@ import { Passenger } from '../../models/passenger.interface';
 import { Baggage } from '../../models/baggage.interface';
 
 @Component({
-    selector: 'passenger-form',
-    styleUrls: ['passenger-form.component.scss'],
-    template: `
+  selector: 'passenger-form',
+  styleUrls: ['passenger-form.component.scss'],
+  template: `
     <form #form="ngForm" novalidate>
       {{ detail | json }}
 
@@ -68,35 +68,36 @@ import { Baggage } from '../../models/baggage.interface';
         </select>
       </div>
 
-      <div>{{ form.value | json }}</div>
-      <div>Valid: {{ form.valid | json }}</div>
-      <div>Invalid: {{ form.invalid | json }}</div>
+      <button type="submit" [disabled]="form.invalid">
+        Update passenger
+      </button>
+
     </form>
   `
 })
 export class PassengerFormComponent {
 
-    @Input()
-    detail: Passenger;
+  @Input()
+  detail: Passenger;
 
-    baggage: Baggage[] = [{
-        key: 'none',
-        value: 'No baggage'
-    }, {
-        key: 'hand-only',
-        value: 'Hand baggage'
-    }, {
-        key: 'hold-only',
-        value: 'Hold baggage'
-    }, {
-        key: 'hand-hold',
-        value: 'Hand and hold baggage'
-    }];
+  baggage: Baggage[] = [{
+    key: 'none',
+    value: 'No baggage'
+  },{
+    key: 'hand-only',
+    value: 'Hand baggage'
+  },{
+    key: 'hold-only',
+    value: 'Hold baggage'
+  },{
+    key: 'hand-hold',
+    value: 'Hand and hold baggage'
+  }];
 
-    toggleCheckIn(checkedIn: boolean) {
-        if (checkedIn) {
-            this.detail.checkInDate = Date.now();
-        }
+  toggleCheckIn(checkedIn: boolean) {
+    if (checkedIn) {
+      this.detail.checkInDate = Date.now();
     }
+  }
 
 }
