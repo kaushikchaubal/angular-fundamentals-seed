3e61530d2bba58096709898a9379b44bc619bf66
Kaushik Chaubal <kaushikchaubal@gmail.com>
2018-07-09 17:43:12 +0100

    Adding nav and routerlinks

--- app/app.module.ts
+++ app/app.module.ts
@@ -5,14 +5,17 @@ import {PassengerDashboardModule} from './passenger-dashboard/passenger-dashboar
 import {RouterModule, Routes} from '@angular/router';
 import {AppComponent} from './app.component';
 import { HomeComponent } from './home.component';
+import { NotFoundComponent } from './not-found.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent, pathMatch: 'full'}
+  {path: '', component: HomeComponent, pathMatch: 'full'},
+  {path:'**', component: NotFoundComponent}
 ]
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent  
+    HomeComponent,
+    NotFoundComponent 
   ], 
   bootstrap: [AppComponent],
   imports: [
