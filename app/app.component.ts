45ae00b3fe14be7066380f92c42988692f82837a
Kaushik Chaubal <kaushikchaubal@gmail.com>
2018-07-09 11:47:08 +0100

    Four ways of dynamically pulling data - ngCladd & ngStyle

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,33 +1,84 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
+
+interface Passenger {
+  id: number,
+  fullname: string,
+  checkedIn: boolean
+}
 
 @Component({
   selector: 'app-root',
   styleUrls: ['app.component.scss'],
   template: `
-    <input 
-    [value]="title"
-    (input) = "handle($event)">
+    <div class="app">
 
-    <template [ngIf] = "title.length > 2">
-      <div>
-        Searching for ... {{ title }}
-      </div>
-    </template>
+    <h3>#1 Airline Passengers</h3>
+    <ul>
+      <li *ngFor="let passenger of passengers; let i = index;">
+        <span
+          class="status"
+          [class.checked-in]="passenger.checkedIn"></span>
+        {{ i }}: {{ passenger.fullname }}
+      </li>
+    </ul>
 
-    <div *ngIf="title.length > 2">
-      Searching for ... {{ title }}
+    <h3>#2 Airline Passengers</h3>
+    <ul>
+      <li *ngFor="let passenger of passengers; let i = index;">
+        <span
+          class="status"
+          [ngClass]="{
+            'checked-in': passenger.checkedIn,
+            'checked-out': !passenger.checkedIn
+          }"></span>
+        {{ i }}: {{ passenger.fullname }}
+      </li>
+    </ul>
+
+      <h3>#3 Airline Passengers</h3>
+      <ul>
+        <li *ngFor="let passenger of passengers; let i = index;">
+          <span
+            class="status"
+            [style.backgroundColor]="(passenger.checkedIn ? '#2ecc71' : '#c0392b')"></span>
+          {{ i }}: {{ passenger.fullname }}
+        </li>
+      </ul>
+
+      <h3>#4 Airline Passengers</h3>
+      <ul>
+        <li *ngFor="let passenger of passengers; let i = index;">
+          <span
+            class="status"
+            [ngStyle]="{
+              backgroundColor: (passenger.checkedIn ? '#2ecc71' : '#c0392b')
+            }"></span>
+          {{ i }}: {{ passenger.fullname }}
+        </li>
+      </ul>
     </div>
   `
 })
-
 export class AppComponent {
-  title: string;
-
-  constructor() {
-    this.title = '';
-  }
-
-  handle(event: any) {
-    this.title = event.target.value;
-  }
+  passengers: Passenger[] = [{
+    id: 1,
+    fullname: 'Stephen',
+    checkedIn: true
+  }, {
+    id: 2,
+    fullname: 'Rose',
+    checkedIn: false
+  }, {
+    id: 3,
+    fullname: 'James',
+    checkedIn: true
+  }, {
+    id: 4,
+    fullname: 'Louise',
+    checkedIn: true
+  }, {
+    id: 5,
+    fullname: 'Tina',
+    checkedIn: false
+  }];
 }
