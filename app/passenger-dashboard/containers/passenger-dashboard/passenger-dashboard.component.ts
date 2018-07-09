a905911d8c08efbf8833b00f53315fe6318fd41d
Kaushik Chaubal <kaushikchaubal@gmail.com>
2018-07-09 14:52:28 +0100

    Adding PassengerService

--- app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
+++ app/passenger-dashboard/containers/passenger-dashboard/passenger-dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Passenger } from '../../models/passenger.interface';
+import { PassengerDashboardService } from '../../passenger-dashboard.service';
 
 @Component({
     selector: 'passenger-dashboard',
@@ -21,39 +22,9 @@ import { Passenger } from '../../models/passenger.interface';
 })
 export class PassengerDashboardComponent implements OnInit {
     passengers: Passenger[];
-    constructor() { }
+    constructor(private passengerService: PassengerDashboardService) { }
     ngOnInit() {
-        this.passengers = [{
-            id: 1,
-            fullname: 'Stephen',
-            checkedIn: true,
-            checkInDate: 1490742000000,
-            children: null
-        }, {
-            id: 2,
-            fullname: 'Rose',
-            checkedIn: false,
-            checkInDate: null,
-            children: [{ name: 'Ted', age: 12 }, { name: 'Chloe', age: 7 }]
-        }, {
-            id: 3,
-            fullname: 'James',
-            checkedIn: true,
-            checkInDate: 1491606000000,
-            children: null
-        }, {
-            id: 4,
-            fullname: 'Louise',
-            checkedIn: true,
-            checkInDate: 1488412800000,
-            children: [{ name: 'Jessica', age: 1 }]
-        }, {
-            id: 5,
-            fullname: 'Tina',
-            checkedIn: false,
-            checkInDate: null,
-            children: null
-        }];
+        this.passengers = this.passengerService.getPassengers();
     }
     handleEdit(event) {
         // Not implemented because I don't see value in adding the logic here
--- app/passenger-dashboard/passenger-dashboard.module.ts
+++ app/passenger-dashboard/passenger-dashboard.module.ts
@@ -3,6 +3,7 @@ import {CommonModule} from '@angular/common'
 import {PassengerDashboardComponent} from './containers/passenger-dashboard/passenger-dashboard.component'
 import {PassengerCountComponent} from './components/passenger-count/passenger-count.component'
 import {PassengerDetailComponent} from './components/passenger-detail/passenger-detail.component'
+import { PassengerDashboardService } from './passenger-dashboard.service';
 
 
 @NgModule({
@@ -16,7 +17,11 @@ import {PassengerDetailComponent} from './components/passenger-detail/passenger-
     ], 
     exports: [
         PassengerDashboardComponent
+    ],
+    providers: [
+        PassengerDashboardService
     ]
+
 
 })
 export class PassengerDashboardModule {}
