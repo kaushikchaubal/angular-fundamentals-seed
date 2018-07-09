18e0c14c91b565c9ec51abded9aed42448d37924
Kaushik Chaubal <kaushikchaubal@gmail.com>
2018-07-09 17:35:10 +0100

    Adding first route (HomeComponent)

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,14 +2,23 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser'
 import {CommonModule} from '@angular/common'
 import {PassengerDashboardModule} from './passenger-dashboard/passenger-dashboard.module'
+import {RouterModule, Routes} from '@angular/router';
 import {AppComponent} from './app.component';
+import { HomeComponent } from './home.component';
 
+const routes: Routes = [
+  {path: '', component: HomeComponent, pathMatch: 'full'}
+]
 @NgModule({
-  declarations: [AppComponent], 
+  declarations: [
+    AppComponent,
+    HomeComponent  
+  ], 
   bootstrap: [AppComponent],
   imports: [
     BrowserModule,
     CommonModule,
+    RouterModule.forRoot(routes),
     PassengerDashboardModule
   ]
 })
