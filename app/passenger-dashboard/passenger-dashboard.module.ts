d2101a724b1f2301cc997a0bac5923db84686cf0
Kaushik Chaubal <kaushikchaubal@gmail.com>
2018-07-09 17:51:20 +0100

    Adding dynamix routes

--- app/passenger-dashboard/passenger-dashboard.module.ts
+++ app/passenger-dashboard/passenger-dashboard.module.ts
@@ -11,8 +11,14 @@ import { PassengerFormComponent } from './components/passenger-form/passenger-fo
 import { RouterModule, Routes } from '@angular/router'
 
 const routes: Routes = [
-    {path: 'passengers', component: PassengerDashboardComponent}
-]
+    {
+      path: 'passengers',
+      children: [
+       { path: '', component: PassengerDashboardComponent },
+       { path: ':id', component: PassengerViewerComponent }
+      ]
+    }
+  ];
 
 @NgModule({
     declarations: [
